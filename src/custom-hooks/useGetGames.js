4c2d8ecf2f9569f05d7ade53f459d283df80daae
Carlos Abarca <45491971+carlos-alejandro98@users.noreply.github.com>
2022-02-20 23:43:20 -0300

    correction

--- src/custom-hooks/useGetGames.js
+++ src/custom-hooks/useGetGames.js
@@ -13,7 +13,6 @@ const useGetGames = () => {
             const response = await axios.get(
                 "https://server-final-project-node.herokuapp.com/api/games/"
             );
-            console.log(response.data);
             setGames(response.data);
             setError(false);
             setLoading(false);
